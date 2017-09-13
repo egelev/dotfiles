5568aba792812a69c823c69d4261d7ed33b3c0ec
Emil Gelev <emil.gelev@gmail.com>
2017-09-13 11:59:16 +0300

    Set default editor to vim

--- bashrc.d/bash_env_vars.sh
+++ bashrc.d/bash_env_vars.sh
@@ -23,9 +23,14 @@ export JAVA_ROOT=$TOOLS/java
 
 export PATH=$HOME/bin:$PATH
 
+# Set default editor
+export VISUAL=vim
+export EDITOR="${VISUAL}"
+
 # Default vim daylight interval
 export VIM_DAYLIGHT_START="8:00"
 export VIM_DAYLIGHT_END="18:00"
+
 
 # ==============================================================================
 # End of actual script
