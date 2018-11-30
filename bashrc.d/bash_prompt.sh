0c7cfec22d8f15055f935b7cb2b17334ac6f38d1
Emil Gelev <egelev@ext.uber.com>
2018-11-30 15:19:22 +0200

    Refactor bash prompt
    * Refactor bash_prompt.sh
    * Append virtual env to the prompt
    * Remove checks for xterm

--- bashrc.d/bash_prompt.sh
+++ bashrc.d/bash_prompt.sh
@@ -10,54 +10,86 @@ __BASH_PROMPT__="__BASH_PROMPT__"
 
 __BASH_PROMPT_SCRIPT_DIR__=$( cd -L $( dirname $(readlink -f "${BASH_SOURCE[0]}") ) && pwd  )
 
+source ${__BASH_PROMPT_SCRIPT_DIR__}/bash_colors.sh
 source ${__BASH_PROMPT_SCRIPT_DIR__}/bash_env_vars.sh
 source ${__BASH_PROMPT_SCRIPT_DIR__}/git-prompt.sh
-source ${__BASH_PROMPT_SCRIPT_DIR__}/bash_colors.sh
 
-# set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
 
-# set a fancy prompt (non-color, unless we know we "want" color)
-case "${TERM}" in
-    xterm-color) color_prompt=yes;;
-esac
-
-# uncomment for a colored prompt, if the terminal has the capability; turned
-# off by default to not distract the user: the focus in a terminal window
-# should be on the output of commands, not on the prompt
-force_color_prompt=yes
-
-if [ -n "${force_color_prompt}" ]; then
-    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-	# We have color support; assume it's compliant with Ecma-48
-	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-	# a case would tend to support setf rather than setaf.)
-	color_prompt=yes
-    else
-	color_prompt=
+getSshConnectionPrefix() {
+    if [[ -n ${SSH_CONNECTION} ]]
+    then
+	local LOCAL_IP=$(echo ${SSH_CONNECTION} | tr -s ' ' | cut -d' ' -f 3)
+	echo "(\[${IWhite}\]${LOCAL_IP})"
     fi
-fi
+}
 
-evalPromptDirPart(){
+getChrootPromptPrefix() {
+    # set variable identifying the chroot you work in (used in the prompt below)
+    if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+	debian_chroot=$(cat /etc/debian_chroot)
+    fi
+    echo "${debian_chroot:+($debian_chroot)}"
+}
+
+getDirectoryToken(){
     local PROMPT_DIR_PART=`pwd`
     [[ ${PROMPT_DIR_PART} == ${WS}* ]] && PROMPT_DIR_PART=~/ws${PROMPT_DIR_PART#$WS}
     [[ ${PROMPT_DIR_PART} == ${HOME}* ]] && PROMPT_DIR_PART=~${PROMPT_DIR_PART#$HOME}
     printf ${PROMPT_DIR_PART}
 }
 
-unset color_prompt force_color_prompt
+getStatusColouredToken() {
+    echo "\[${CMD_DEPENDENT_COLOR}\]"
+}
 
+getPromptSuffixSymbol() {
+    echo "\$"
+}
+
+getVirtualEnvName() {
+    if [ ! -z ${VIRTUAL_ENV+x} ]
+    then
+	echo "|$(basename ${VIRTUAL_ENV})"
+    fi
+}
+
+appendSuffixSymbolToPrompt() {
+    PS1="${PS1}\[${White}\]$(getPromptSuffixSymbol)"
+}
+
+appendSpaceToPrompt() {
+    PS1="${PS1} "
+}
+
+defaultBashPrompt(){
+        result=
+	for token in $(getSshConnectionPrefix) $(getChrootPromptPrefix) "$(getStatusColouredToken)\t" "\[${White}\]:" "\[${Blue}\]$(getDirectoryToken)" "\[${BPurple}\]\$(__git_ps1)" "\[${Cyan}\]$(getVirtualEnvName)"
+	do
+	    result="${result}${token}"
+
+	done
+	echo "${result}"
+}
+
+
+# Overridable
 getBashPromptColorOnSuccess() {
     echo "${BIGreen}"
 }
 
+# Overridable
 getBashPromptColorOnFailure() {
     echo "${BIRed}"
 }
 
-getBashPromptColorDependingOnExitStatus() {
+# Overridable
+setBashPrompt() {
+    PS1="$(defaultBashPrompt)"
+    appendSuffixSymbolToPrompt
+    appendSpaceToPrompt
+}
+
+_getBashPromptColorDependingOnExitStatus() {
     local LAST_CMD_EXIT_CODE="$?"
     if [[ ${LAST_CMD_EXIT_CODE} == 0 ]]
     then
@@ -67,37 +99,13 @@ getBashPromptColorDependingOnExitStatus() {
     fi
 }
 
-defaultBashPrompt(){
-        PS1="${debian_chroot:+($debian_chroot)}\[${CMD_DEPENDENT_COLOR}\]\t\[${White}\]:\[${Blue}\]$(evalPromptDirPart)\[${BPurple}\]\$(__git_ps1)\[${White}\]\$ "
-	if [[ -n ${SSH_CONNECTION} ]]
-	then
-	    local LOCAL_IP=$(echo ${SSH_CONNECTION} | tr -s ' ' | cut -d' ' -f 3)
-	    PS1="(\[${IWhite}\]${LOCAL_IP}):${PS1}"
-	fi
-}
-
-# Overridable
-setBashPrompt() {
-    defaultBashPrompt
-}
-
 __bashColorifiedPromptFunction() {
     # Export the cmd dependent color to the overridable function
-    export CMD_DEPENDENT_COLOR=$(getBashPromptColorDependingOnExitStatus)
+    export CMD_DEPENDENT_COLOR=$(_getBashPromptColorDependingOnExitStatus)
     setBashPrompt
 }
 
 PROMPT_COMMAND=__bashColorifiedPromptFunction
-
-# If this is an xterm set the title to user@host:dir
-case "${TERM}" in
-xterm*|rxvt*)
-    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]${PS1}"
-    ;;
-*)
-    ;;
-esac
-
 
 
 # ==============================================================================
